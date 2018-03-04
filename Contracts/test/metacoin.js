810dc3d1584ffcac5ec30d95b9703dea88a94550
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2018-03-04 07:08:05 +0530

    Added example code

--- Contracts/test/metacoin.js
+++ Contracts/test/metacoin.js
@@ -8,6 +8,7 @@ contract('MetaCoin', function(accounts) {
       assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
     });
   });
+  /** 
   it("should call a function that depends on a linked library", function() {
     var meta;
     var metaCoinBalance;
@@ -59,5 +60,5 @@ contract('MetaCoin', function(accounts) {
       assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
       assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
     });
-  });
+  }); **/
 });
